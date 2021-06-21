aba54cd20ec53374830620d877e15a5c4c0dd150
sergey <korolsergey.p@gmail.com>
2021-06-21 13:33:18 +0300

    new site

--- js/show-text.js
+++ js/show-text.js
@@ -4,6 +4,7 @@ const showBtn = document.querySelector('.about__show-button');
 const showText = document.querySelector('.about__description-text-wrapper');
 
 showBtn.addEventListener('click', () => {
-    showText.classList.add('show');
-    showBtn.style.display = 'none';
+    showText.classList.toggle('show');
+    showBtn.classList.toggle('_move-button');
+
 });
--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,25 @@
-const contentWrapper = document.querySelector('.masters__content');
+let offsetSlider = 0;
+
+const btnPrev = document.querySelector('.master__prev-new');
+const btnNext = document.querySelector('.master__next-new');
+/* const contentWrapper = document.querySelector('.masters__content');
+ */
+const container = document.querySelector('.masters__container');
+const sliderWrapper = document.querySelector('.masters__slider-wrapper');
+
+
+btnNext.addEventListener('click', () => {
+    offsetSlider += 1170;
+    if (offsetSlider > 2340) {
+        offsetSlider = 0;
+    }
+    sliderWrapper.style.left = -offsetSlider + 'px';
+});
+
+btnPrev.addEventListener('click', () => {
+    offsetSlider -= 1170;
+    if (offsetSlider < 0) {
+        offsetSlider = 2340;
+    }
+    sliderWrapper.style.left = -offsetSlider + 'px';
+});
